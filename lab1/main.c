6177270b496cd3ec83ab17b346a10f8845b31a95
Anton <vorobiov.anton@lll.kpi.ua>
2023-10-07 10:19:44 +0300

    feat: Add m and n validation

--- lab1/main.c
+++ lab1/main.c
@@ -4,7 +4,7 @@
 /**
  * Воробйов Антон, ІО-12, варіант 4
  *
- * z_1 = ((m - 1) * sqrt(m) - (n-1) * sqrt(n)) / (sqrt(m^3 * n) + n * m + m^2 - m)
+ * z_1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(m^3 * n) + n * m + m^2 - m)
  * z_2 = (sqrt(m) - sqrt(n)) / m
  */
 
@@ -13,13 +13,20 @@ int main(void)
   double m, n, z1, z2;
   printf("m = ");
   scanf("%lf", &m);
-  if (m == 0)
+  // Програма обчислює `sqrt(m)` та виконує ділення на `m`
+  if (m <= 0)
   {
-    printf("m can't be equal to 0.");
+    printf("`m` must be greater than 0.");
     return 0;
   }
   printf("n = ");
   scanf("%lf", &n);
+  // Програма обчислює `sqrt(n)`
+  if (n < 0)
+  {
+    printf("`n` must be greater than or equal to 0.");
+    return 0;
+  }
   z1 = ((m - 1) * sqrt(m) - (n - 1) * sqrt(n)) / (sqrt(pow(m, 3) * n) + m * n + pow(m, 2) - m);
   z2 = (sqrt(m) - sqrt(n)) / m;
   printf("z1 = %lf\n", z1);
